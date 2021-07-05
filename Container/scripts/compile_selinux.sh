f27a0b932ab92397d75c3e7b94be188514dfae86
Matt Walker <sylinsic@localhost.localdomain>
2021-07-05 16:26:51 +0100

    Functional SELinux policy

--- Container/scripts/compile_selinux.sh
+++ Container/scripts/compile_selinux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ../policies
-sudo make -f /usr/share/selinux/devel/Makefile priote.pp
-sudo semodule -i priote.pp
+make -f /usr/share/selinux/devel/Makefile priote.pp 	# Compile the selinux policy
+sudo semodule -i priote.pp				# Insert the created module
+sudo semanage port -a -t priote_port_in_t -p tcp 3330	# Add the port for our web server
